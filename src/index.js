2a19c4edba4afa125131059954a1e11435fc0865
Marcio Silva <marcionps@proton.me>
2023-05-29 21:07:47 -0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
 const arrayDiff = (L1, L2) => {
-  out = []
-  if (L2.length === 0) {
-    return L1;
-  }
-
-  out = [...L1]
-
-  for (elem2 of L2) {
-    if (L1.includes(elem2)) {
-      out = out.filter(x => x !== elem2)
-    }
-  }
-
-  return out;
+  return L1.filter(x => !L2.includes(x));
 };
 
 module.exports = {
